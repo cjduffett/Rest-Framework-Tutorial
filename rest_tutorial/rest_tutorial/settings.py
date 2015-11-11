937f48308b190af217c718c9c9b58c4844b95e97
Carlton Duffett <cjduffett@comcast.net>
2015-11-10 22:38:25 -0500

    Completed Tutorial

--- rest_tutorial/rest_tutorial/settings.py
+++ rest_tutorial/rest_tutorial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'snippets',
 )
 
@@ -102,3 +103,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST Framework Config
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
--- rest_tutorial/rest_tutorial/urls.py
+++ rest_tutorial/rest_tutorial/urls.py
@@ -17,6 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('snippets.urls')),
+    url(r'^admin/',
+        include(admin.site.urls)),
+    url(r'^',
+        include('snippets.urls')),
+    url(r'^auth/',
+        include('rest_framework.urls',
+        namespace='rest_framework')),
 ]
